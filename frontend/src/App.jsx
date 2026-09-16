89bee37d72674d28bb39ce1358e0eff8d89d623e
Ashbin Santhosh <ashbinsanthosh2@gmail.com>
2026-09-16 21:08:57 +0530

    initial lms project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="min-h-screen flex items-center justify-center">
+
+      <h1 className="text-4xl font-bold">
+
+        LMS Project
+
+      </h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
